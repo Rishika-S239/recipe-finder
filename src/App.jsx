06e9c38ca172846d0fe0e17e25b4a8ea7c24cc1e
Rishika Sahay <sahayrishika23@gmail.com>
2025-08-19 22:40:26 +0530

    Fix import path for Recipes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import { useState } from 'react'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import {Routes, Route} from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Link } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './components/Home';
-import Recipes from './components/recipes';
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Recipes from "./components/Recipes";
 
 function App() {
-
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/recipes/:id" element={<Recipes />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
